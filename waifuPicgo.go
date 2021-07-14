5b5abd20f31599ee7cbf7a5289ce28997f8590ef
RedAura <86167689+MoeZilla@users.noreply.github.com>
2021-07-14 20:20:53 +0530

    Create waifuPicgo.go

---
+++ waifuPicgo.go
@@ -0,0 +1,51 @@
+package waifuPicgo
+
+import (
+	"encoding/json"
+        "errors"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func sfw(category  string)(err error){
+        var waifu struct {
+		Url string `json:"url"`
+	}
+        URL := fmt.Sprintf("https://api.waifu.pics/sfw/", category)
+	resp, err := http.Get(URL)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	err = json.Unmarshal(body, &waifu)
+	if err != nil {
+		return nil, err
+	}
+	return waifu.Url, nil
+}
+
+func nsfw(category  string)(err error){
+        var waifu struct {
+		img string `json:"url"`
+	}
+        URL := fmt.Sprintf("https://api.waifu.pics/nsfw/", category)
+	resp, err := http.Get(URL)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	err = json.Unmarshal(body, &waifu)
+	if err != nil {
+		return nil, err
+	}
+	return waifu.img, nil
+}
